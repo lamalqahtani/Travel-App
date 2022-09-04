c84d121d39e04bc49977742cf62cd95587bfd4a0
Lamyaa Q <lamiaa@Lamyaas-MacBook-Pro.local>
2022-09-04 23:14:39 +0300

    enhancement in coding + comment removed + add modal and some styling

--- src/client/index.js
+++ src/client/index.js
@@ -1,68 +1,54 @@
-// import {getFetch} from './js/getFetch';
 import {geonameAPI} from './js/geoname';
 import {weatherbit} from './js/weatherbit';
 import {pixabay} from './js/pixabay';
 
 
 const formElement = document.getElementById('form');
-const content = document.getElementById('content');
+const modalTitle = document.getElementById('exampleModalLabel');
+const modalBodyContainer = document.getElementById('modal-body-id');
+const modalBody = document.getElementById('modal-body-id');
+const saveButton = document.getElementById('save');
 formElement.addEventListener('submit',async (event)=>{
     event.preventDefault();
-    //alert('hello!!');
-    // getFetch();
-    // let countryName = 'paris';
+
     let date = document.getElementById('date').value;
-    // console.log("date: "+ date);
     let countryName = document.getElementById('country').value;
     let coordenates = await geonameAPI(countryName);
     
     console.log(coordenates.postalCodes[0].lng);
     console.log(coordenates.postalCodes[0].lat);
     console.log(coordenates.postalCodes[0].placeName);
+    modalTitle.textContent = 'Trip to ' + countryName;
     
     let forcast = await weatherbit(coordenates.postalCodes[0].lng,coordenates.postalCodes[0].lat);
     console.log('forcast data: ');
     console.log(forcast);
+
+    // current date with yyyy-mm-dd format => new Date().toISOString().slice(0, 10)
+    let currentDate = new Date().toISOString().slice(0, 10);
+    // console.log('currentDate '+ currentDate);
+    // console.log('Date '+ date);
+    if(currentDate > date){
+        modalBody.innerHTML = 'Date expired';
+        saveButton.style.display = 'none';
+        return;
+    }
+
+    let found = false;
     for (let i = 0; i < forcast.data.length; i++) {
         if(date == forcast.data[i].datetime){
-            content.innerHTML = `High: ${forcast.data[i].high_temp} <br> Low: ${forcast.data[i].low_temp} <br> Description: ${forcast.data[i].weather.description}`;
-            console.log('high: '+ forcast.data[i].high_temp);
-            console.log('low: '+ forcast.data[i].low_temp);
-            console.log('description: '+ forcast.data[i].weather.description);
-            
-        }else{
-            console.log('sorry, no data available')
-        }
-        
+            modalBody.innerHTML = `<p> High: ${forcast.data[i].high_temp} <br> Low: ${forcast.data[i].low_temp} <br> Description: ${forcast.data[i].weather.description} </p>`; 
+            found = true;
+        }  
     }
-    // console.log('high: '+ forcast.data[0].high_temp);
-    // console.log('low: '+ forcast.data[0].low_temp);
-    // console.log('description: '+ forcast.data[0].weather.description);
-    
-    let imageData = await pixabay(countryName);
-    console.log(imageData);
-    console.log(imageData.hits[0].previewURL);
-    let image = document.createElement('img');
-    image.setAttribute('src',imageData.hits[0].previewURL);
-    content.appendChild(image);
+    if(found){
+        let imageData = await pixabay(countryName);
+        let placeImage = document.createElement('img');
+        placeImage.setAttribute('src',imageData.hits[0].previewURL);
+        modalBodyContainer.appendChild(placeImage); 
+    }else{
+        modalBody.innerHTML = `<p> No forcast data found</p>`; 
+    }
+
+
 });
-// document.getElementById('btn').addEventListener('click',async ()=>{
-//     //alert('hello!!');
-//     // getFetch();
-//     let countryName = 'paris';
-//     let coordenates = await geonameAPI(countryName);
-//     console.log(coordenates.postalCodes[0].lng);
-//     console.log(coordenates.postalCodes[0].lat);
-//     console.log(coordenates.postalCodes[0].placeName);
-
-//     let forcast = await weatherbit(coordenates.postalCodes[0].lng,coordenates.postalCodes[0].lat);
-//     console.log('forcast data: ');
-//     console.log(forcast);
-//     console.log('high: '+ forcast.data[0].high_temp);
-//     console.log('low: '+ forcast.data[0].low_temp);
-//     console.log('description: '+ forcast.data[0].weather.description);
-
-//     let imageData = await pixabay(countryName);
-//     console.log(imageData);
-//     console.log(imageData.hits[0].previewURL);
-// })
